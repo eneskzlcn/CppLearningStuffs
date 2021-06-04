18551f870a9f5ffd23b400fb104fd738d5eb3a14
Enes Kızılcan <nazifenes.kizilcin@stu.fsm.edu.tr>
2021-06-05 02:41:29 +0300

    if the reference value changes, it just changes the value which it is references

--- CppLearnings/CppLearnings/Main.cpp
+++ CppLearnings/CppLearnings/Main.cpp
@@ -16,8 +16,29 @@ int main()
 	// void* ptr = nullptr;
 
 	int var = 8;
+	int var1 = 10;
 	multiply(var);
 	LOG(var);
+
+	//when you create a reference, you need to directly declare the reference
+	//example:
+	//int& ref = var;
+	// you can not do ex:
+	/*int& ref1;
+	ref1 = var;*/
+	//because of that a ref directly refrences to this variable adress and you can not change it like:
+
+	/*int& ref = var;
+	ref = var1;*/
+
+	// if you do this, not the reference changes. Both of the variable values will be the value which var1 has ( 10)
+	//uncomment below code and try it:
+
+	//int& ref = var;
+	//ref = var1;
+	//LOG(var);
+	//LOG(var1);
+
 	//void* ptr = &var;
 	//LOG(ptr);
 	std::cin.get();
