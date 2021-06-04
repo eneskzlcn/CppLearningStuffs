817387807cc07e75dabeffc960ec5980c299db96
Enes Kızılcan <nazifenes.kizilcin@stu.fsm.edu.tr>
2021-06-05 02:35:31 +0300

    working with references on functions

--- CppLearnings/CppLearnings/Main.cpp
+++ CppLearnings/CppLearnings/Main.cpp
@@ -1,9 +1,25 @@
 #include <iostream>
 
+#define LOG(x) std::cout << x << std::endl
 
+void multiply(int& x)
+{
+	x++;
+}
 
 int main()
 {
+	LOG("me");
+	//all of the definitions below , means it is a null pointer.
+	//void* ptr = 0;
+	//void * ptr = NULL;
+	// void* ptr = nullptr;
 
+	int var = 8;
+	multiply(var);
+	LOG(var);
+	//void* ptr = &var;
+	//LOG(ptr);
+	std::cin.get();
 	return 0;
 }
