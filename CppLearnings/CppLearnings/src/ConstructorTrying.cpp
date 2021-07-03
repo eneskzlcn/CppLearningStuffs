a35643a59425421a2f2d2076c0c2d15f80569207
Enes Kızılcin <nazifenes.kizilcin@gmail.com>
2021-07-03 18:17:11 +0300

    some Type Punning operations in cpp made in TypePunning.cpp

--- CppLearnings/CppLearnings/src/ConstructorTrying.cpp
+++ CppLearnings/CppLearnings/src/ConstructorTrying.cpp
@@ -1,19 +1,19 @@
-
-#include <iostream>
-
-class Car
-{
-public:
-	Car(int x, int y) //this kind of constructor works fine....
-		:pos_x(x), pos_y(y) {}
-
-	int GetPosX() { return pos_x; }
-	int GetPosY() { return pos_y; }
-private:
-	int pos_x, pos_y;
-};
-
-int main() {
-	Car * car = new Car(3, 5);
-	std::cout << " X: " << car->GetPosX() << " ," << " Y: " << car->GetPosY() << std::endl;
-}
+//
+//#include <iostream>
+//
+//class Car
+//{
+//public:
+//	Car(int x, int y) //this kind of constructor works fine....
+//		:pos_x(x), pos_y(y) {}
+//
+//	int GetPosX() { return pos_x; }
+//	int GetPosY() { return pos_y; }
+//private:
+//	int pos_x, pos_y;
+//};
+//
+//int main() {
+//	Car * car = new Car(3, 5);
+//	std::cout << " X: " << car->GetPosX() << " ," << " Y: " << car->GetPosY() << std::endl;
+//}
