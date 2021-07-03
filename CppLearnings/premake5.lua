e0ea2b0b747bf310067e4165ad97a2fb40b8ecb4
Enes Kızılcin <nazifenes.kizilcin@gmail.com>
2021-07-03 17:28:30 +0300

    a project build system added to project
    writed a premake file that generates the project for vs2019 and in this commit , there is just the body of project here ( cleaned from binary and object files)

---
+++ CppLearnings/premake5.lua
@@ -0,0 +1,39 @@
+workspace "CppLearnings"
+    architecture "x64"
+    startproject "CppLearnings"
+    configurations
+    {
+        "Debug",
+        "Release"
+    }
+
+outputdir = "%{cfg.buildcfg}-%{cfg.system}-%{cfg.architecture}"
+
+project "CppLearnings"
+    location "CppLearnings"
+    kind "ConsoleApp"
+    language "C++"
+
+    targetdir ("bin/" ..outputdir.. "/%{prj.name}")
+    objdir ("bin-int/" ..outputdir.. "/%{prj.name}")
+
+    files
+    {
+        "%{prj.name}/src/**.h",
+        "%{prj.name}/src/**.cpp"
+    }
+
+    filter "system:windows"
+        cppdialect "C++17"
+        staticruntime "On"
+        systemversion "latest"
+    
+        filter "configurations:Debug"
+        defines "_DEBUG"
+        symbols "On"
+
+    filter "configurations:Release"
+        defines "_RELEASE"
+        optimize "On"
+    
+    
